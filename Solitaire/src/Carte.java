602dcdf12c814812c95b4ba96da5ec752bdc8cdf
Luc-Leveque <31991589+Luc-Leveque@users.noreply.github.com>
2017-11-07 14:48:35 +0100

    Creation class et debut classe carte

---
+++ Solitaire/src/Carte.java
@@ -0,0 +1,37 @@
+public class Carte {
+
+	private  int val ;
+    private int type ;
+    
+    public Carte(int val , int type )
+    {
+        this.val=val ; 
+        this.type=type ; 
+    }
+    
+    public int getVal()
+    {
+        return val ; 
+    }
+    
+    public int getType()
+    {
+        return type ; 
+    }
+    
+    public void setVal(int val)
+    {
+            this.val = val;
+    }
+
+    public void setType(int type)
+    {
+            this.type = type;
+    }
+    
+    public String toString()
+    {
+    	
+    }
+
+}
